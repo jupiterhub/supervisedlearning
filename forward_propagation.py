4b41e0be4a7d8b3b3b1c0337f304ebb04a54fba5
jupiterhub <4177229+jupiterhub@users.noreply.github.com>
2023-04-06 12:08:48 +0900

    feat: added cross-entropy loss to work with softmax

--- forward_propagation.py
+++ forward_propagation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mnist as data
 
 
 def sigmoid(z):
@@ -25,3 +26,18 @@ def classify(x, w1, w2):
     y_hat = forward(x, w1, w2)
     labels = np.argmax(y_hat, axis=1)
     return labels.reshape(-1, 1)
+
+
+# cross-entropy loss, a simpler formula compared to log-loss
+# works better with SOFTMAX
+def loss(y, y_hat):
+    return -np.sum(y * np.log(y_hat)) / y.shape[0]
+
+
+def report(iteration, x_train, y_train, x_test, y_test, w1, w2):
+    y_hat = forward(x_train, w1, w2)
+    training_loss = loss(y_train, y_hat)
+    classifications = classify(x_test, w1, w2)
+    accuracy = np.average(classifications == y_test) * 100.0
+    print("Iteration: %5d, Loss: %.6f, Accuracy: %.2f%%" %
+          (iteration, training_loss, accuracy))
