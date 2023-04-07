a09e1a0bd31ed1a24ea4736d562b18850d8aea0c
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2023-04-07 11:15:06 +0900

    feat: prepare backpropagation since derivatives of loss is not sufficient for neural-network

---
+++ neuralnetwork.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def softmax(logits):
+    exponentials = np.exp(logits)
+    return exponentials / np.sum(exponentials, axis=1).reshape(-1, 1)
+
+
+def loss(Y, y_hat):
+    return -np.sum(Y * np.log(y_hat)) / Y.shape[0]
+
+
+def prepend_bias(X):
+    return np.insert(X, 0, 1, axis=1)
+
+
+def sigmoid_gradient(sigmoid):
+    return np.multiply(sigmoid, (1 - sigmoid))
