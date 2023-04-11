11ef40e10ae872457d4fdf2f8be28a897278f75c
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2023-04-11 12:46:35 +0900

    feat: backpropagation with dead neuron as an example

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -22,8 +22,59 @@ def sigmoid_gradient(sigmoid):
     return np.multiply(sigmoid, (1 - sigmoid))
 
 
+def forward(x, w1, w2):
+    h = sigmoid(np.matmul(prepend_bias(x), w1))
+    y_hat = softmax(np.matmul(prepend_bias(h), w2))
+    return y_hat, h
+
+
 def back(x, y, y_hat, w2, h):
     w2_gradient = np.matmul(prepend_bias(h).T, (y_hat - y)) / x.shape[0]
     w1_gradient = np.matmul(prepend_bias(x).T, np.matmul(y_hat - y, w2[1:].T)
                             * sigmoid_gradient(h)) / x.shape[0]
     return w1_gradient, w2_gradient
+
+
+def classify(x, w1, w2):
+    y_hat, _ = forward(x, w1, w2)
+    labels = np.argmax(y_hat, axis=1)
+    return labels.reshape(-1, 1)
+
+
+def initialize_weights(n_input_variables, n_hidden_nodes, n_classes):
+    w1_rows = n_input_variables + 1
+    w1 = np.random.randn(w1_rows, n_hidden_nodes) * np.sqrt(1 / w1_rows)
+
+    w2_rows = n_hidden_nodes + 1
+    w2 = np.random.randn(w2_rows, n_classes) * np.sqrt(1 / w2_rows)
+
+    return (w1, w2)
+
+
+def report(iteration, X_train, Y_train, X_test, Y_test, w1, w2):
+    y_hat, _ = forward(X_train, w1, w2)
+    training_loss = loss(Y_train, y_hat)
+    classifications = classify(X_test, w1, w2)
+    accuracy = np.average(classifications == Y_test) * 100.0
+    print("Iteration: %5d, Loss: %.8f, Accuracy: %.2f%%" %
+          (iteration, training_loss, accuracy))
+
+
+def train(x_train, y_train, x_test, y_test, n_hidden_nodes, iterations, lr):
+    n_input_variables = x_train.shape[1]
+    n_classes = y_train.shape[1]
+    w1, w2 = initialize_weights(n_input_variables, n_hidden_nodes, n_classes)
+    for iteration in range(iterations):
+        y_hat, h = forward(x_train, w1, w2)
+        w1_gradient, w2_gradient = back(x_train, y_train, y_hat, w2, h)
+        w1 = w1 - (w1_gradient * lr)
+        w2 = w2 - (w2_gradient * lr)
+        report(iteration, x_train, y_train, x_test, y_test, w1, w2)
+    return w1, w2
+
+
+# example of saturated sigmoid by providing  a large number
+# gradeint of 0 is multiplied with other local gradients, causing entire gradient to beecome 0
+# it doesn't matter how long we train, because this is now a "dead neuron"
+weighted_sum = 1000 * 10
+print(sigmoid(weighted_sum))
