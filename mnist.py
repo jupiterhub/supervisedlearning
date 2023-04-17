68016b7e5fdd85e282c5d9efeffad78c05fd3a35
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2023-04-17 11:55:09 +0900

    feat: separate mnist data to have a validation data. 6k/5k/5k

--- mnist.py
+++ mnist.py
@@ -22,13 +22,6 @@ def prepend_bias(X):
     return np.insert(X, 0, 1, axis=1)
 
 
-# 60000 images, each 785 elements (1 bias + 28 * 28 pixels)
-X_train = prepend_bias(load_images("mnist_data/train-images-idx3-ubyte.gz"))
-
-# 10000 images, each 785 elements, with the same structure as X_train
-X_test = prepend_bias(load_images("mnist_data/t10k-images-idx3-ubyte.gz"))
-
-
 def load_labels(filename):
     # Open and unzip the file of images:
     with gzip.open(filename, 'rb') as f:
@@ -56,8 +49,16 @@ def one_hot_encode(y):
     return encoded_y
 
 
-# 60K labels, each with value 1 if the digit is a five, and 0 otherwise
-Y_train = one_hot_encode(load_labels("mnist_data/train-labels-idx1-ubyte.gz"))
+# X_train/X_validation/X_test: 60K/5K/5K images
+# Each image has 784 elements (28 * 28 pixels)
+X_train = load_images("/mnist_data/train-images-idx3-ubyte.gz")
+X_test_all = load_images("/mnist_data/t10k-images-idx3-ubyte.gz")
+X_validation, X_test = np.split(X_test_all, 2)
 
-# 10000 labels, with the same encoding as Y_train
-Y_test = load_labels("mnist_data/t10k-labels-idx1-ubyte.gz")
+# 60K labels, each a single digit from 0 to 9
+Y_train_unencoded = load_labels("/mnist_data/train-labels-idx1-ubyte.gz")
+# Y_train: 60K labels, each consisting of 10 one-hot-encoded elements
+Y_train = one_hot_encode(Y_train_unencoded)
+# Y_validation/Y_test: 5K/5K labels, each a single digit from 0 to 9
+Y_test_all = load_labels("/mnist_data/t10k-labels-idx1-ubyte.gz")
+Y_validation, Y_test = np.split(Y_test_all, 2)
