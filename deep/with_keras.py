534e7f0898be9263332d8687c742fa9221641d1a
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2023-04-26 09:49:38 +0900

    feat: added Keras library with Echidna dataset

---
+++ deep/with_keras.py
@@ -0,0 +1,29 @@
+from keras import Sequential
+from keras.layers import Dense
+from keras.optimizers.legacy.rmsprop import RMSProp
+from tensorflow.python.keras.utils.np_utils import to_categorical
+
+import echidna as data
+import boundary
+
+X_train = data.X_train
+X_validation = data.X_validation
+# one-hot-encode the data
+Y_train = to_categorical(data.Y_train)
+Y_validation = to_categorical(data.Y_validation)
+
+model = Sequential()
+model.add(Dense(100, activation='sigmoid'))
+# make it into a 4-layer network considered 'deep', since it's more than the shallow 3-layer system
+model.add(Dense(30, activation='sigmoid'))
+model.add(Dense(2, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSProp(learning_rate=0.001),
+              metrics=['accuracy'])
+
+model.fit(X_train, Y_train,
+          validation_data=(X_validation, Y_validation),
+          epochs=30000, batch_size=25)
+
+boundary.show(model, data.X_train, data.Y_train)
